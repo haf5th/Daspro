42aa8f8bbeb29e443a9cd3b19b2aa2e52a2da827
haf5th <hafiflearning@gmail.com>
2024-09-09 21:55:29 +0700

    commit daspro

---
+++ Jb1/BiodataHafif.java
@@ -0,0 +1,16 @@
+public class BiodataHafif {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : Hafif Nurrahmad");
+        System.out.println("NIM      : 244107020176");
+        System.out.println("Kelas    : TI 1D");
+        System.out.println("No Absen : 11");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Alamat   : Sumbermanjing Wetan,Kab Malang");
+        System.out.println("No HP    : 085235499241");
+        System.out.println("Hobi     : Game Online");
+        
+    }
+}
---
+++ Jb2/aaa.java
@@ -0,0 +1,13 @@
+package Jb2;
+
+/**
+ * aaa
+ */
+public class aaa {
+
+    public static void main(String[] args) {
+        int x = 1, b =12;
+        int c = x + b * 0;
+        System.out.println(c);
+    }
+}
